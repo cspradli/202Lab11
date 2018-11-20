4f741e2f5de250147e25cee7f3b7866b1db38241
Caleb Spradlin <calebspradlin@dhcp-87-87.ram.secure.int.wireless.unca.edu>
2018-11-20 16:11:47 -0500

    Made boiler plate code

---
+++ PrintRecursive.java
@@ -0,0 +1,8 @@
+public class PrintRecursive{
+    public static void recursivePrint(int i, int j){
+
+    }
+    public static void main(String[] args) {
+        recursivePrint(1, 4);
+    }
+}
---
+++ TestLinkedList.java
@@ -0,0 +1,72 @@
+public class TestLinkedList<E> { 
+    private Node head ;
+    private Node tail ;
+
+    private class Node { 
+        private E data ;
+        private Node next ;
+        private Node(E data , Node next) { 
+            this.data = data;
+            this.next = next;
+        }
+        private Node(E data) { 
+            this(data, null);
+        }
+        private Node() { 
+            this(null, null);
+        } 
+    }
+
+    public TestLinkedList() { 
+        this.head = new Node(); tail = head;
+    }
+
+    public void add(E item) {
+        head.next = new Node(item, head.next);
+        if (size == 0) tail = head.next;
+    }
+
+    public int size() {
+        // TO DO: Complete the body of this method.
+        return size;
+    }
+
+    public void concat(TestLinkedList<E> list2 ) { 
+        // TO DO: Complete the body of this method.
+    }
+
+    @Override
+    public String toString () {
+    if (size == 0) return "[ ]";
+    StringBuilder sb = new StringBuilder ("["); 
+    Node current = head . next ;
+    while ( current . next != null ) {
+        sb.append(current.data + ", ");
+        current = current.next;
+    }
+    sb.append(current.data + "]\n"); 
+    return sb.toString();
+    }
+
+    public static void main(String [] args) { 
+        TestLinkedList<String> listA = new TestLinkedList<>(); 
+        TestLinkedList<String> listB = new TestLinkedList<>();
+        listA.add("three");
+        listA .add("two");
+        listA .add("one");
+        System.out.println("List A: " + listA);
+        listB.add("six ") ;
+        listB.add("five");
+        listB.add("four");
+        System.out.println("List B: " + listB);
+        listA.concat(listB);
+        System.out.println("Concatenated lists : " + listA);
+        }
+}
+
+
+
+    
+
+
+    
