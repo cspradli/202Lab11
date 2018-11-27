d7f2a64b28d1aba85859e4c0d9e55948a3769413
Caleb Spradlin <cspradli@unca.edu>
2018-11-26 20:11:58 -0500

    prints right but idk wtf to do about second iter.

--- PrintRecursive.java
+++ PrintRecursive.java
@@ -2,33 +2,29 @@ public class PrintRecursive{
     public static void recursivePrint(int a, int b){
         boolean c = false;
         recursivePrint(a, b, c);
-        /*if (a <= b){
-            System.out.println("This was written by call number " + a);
-            recursivePrint(a+1, b);
-        }
-        if ((a-b) == 1){
-            System.out.println("This was ALSO written by call number " + (a-1));
-        }
-        if (a >= b && a < (b)){
-            System.out.println("test print");
-            System.out.println("This was ALSO written by call number " + (a - (a-b)));
-            recursivePrint(a+1, b);
-        }*/
     }
 
     private static void recursivePrint(int a, int b, boolean c){
         if (a == b){
-            System.out.println("This was written by call number " + a);
-            System.out.println("This was ALSO written by call number " + a);
+            System.out.println("   This was written by call number " + a);
+            System.out.println("   This was ALSO written by call number " + a);
             c = true;
             recursivePrint(a-1, b, c);
         }
         if(!c){
+            for(int i = 0; i < (a-1); i++){
+                System.out.print(" ");
+            }
             System.out.println("This was written by call number " + a);
             recursivePrint(a+1, b, c);
         }
-        if (c && a != 0){
+        if (c && a > 0){
+            //System.out.println("test");
+            for(int i = 0; i < (a-1); i++){
+                System.out.print(" ");
+            }
             System.out.println("This was ALSO written by call number " + a);
+            recursivePrint(a-1, b, c);
         }
     }
 
