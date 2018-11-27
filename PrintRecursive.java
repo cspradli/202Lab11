d417635dea63a41ff5b6d16ae11424acaf186367
Caleb Spradlin <cspradli@unca.edu>
2018-11-27 13:35:40 -0500

    Made fixes to Print Recur

--- PrintRecursive.java
+++ PrintRecursive.java
@@ -1,35 +1,15 @@
 public class PrintRecursive{
     public static void recursivePrint(int a, int b){
-        boolean c = false;
-        recursivePrint(a, b, c);
-    }
-
-    private static void recursivePrint(int a, int b, boolean c){
-        if (a == b){
-            for(int i = 0; i < (a-1); i++){
+        if (a <= b){
+            for (int i = 0; i < a-1; i++){
                 System.out.print(" ");
             }
             System.out.println("This was written by call number " + a);
-            for(int i = 0; i < (a-1); i++){
+            recursivePrint(a+1, b);
+            for (int i = 0; i < a-1; i++){
                 System.out.print(" ");
             }
             System.out.println("This was ALSO written by call number " + a);
-            c = true;
-            recursivePrint(a-1, b, c);
-        }
-        else if (!c){
-            for(int i = 0; i < (a-1); i++){
-                System.out.print(" ");
-            }
-            System.out.println("This was written by call number " + a);
-            recursivePrint(a+1, b, c);
-        }
-        else if (c && a > 0){
-            for(int i = 0; i < (a-1); i++){
-                System.out.print(" ");
-            }
-            System.out.println("This was ALSO written by call number " + a);
-            recursivePrint(a-1, b, c);
         }
     }
 
